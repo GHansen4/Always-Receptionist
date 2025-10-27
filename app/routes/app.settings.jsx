841216c7428805c2ff6601fdf17cc28b0aa69d85
GHansen4 <g.hansen17@gmail.com>
2025-10-27 15:19:10 -0400

    Update settings page to use React Router's useLoaderData
    - Changed import from @remix-run/react to react-router
    - Updated for React Router v7 compatibility
    - Maintained all existing functionality

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "react-router";
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
 import { Page, Card, Text, BlockStack } from "@shopify/polaris";
