cc4c9a14b6216e5b99c8ee8d90fb1b627bfeff9e
GHansen4 <g.hansen17@gmail.com>
2025-10-29 09:36:06 -0400

    Clean up react-router.config.js format - Remove JSDoc type annotation for cleaner config - Match official Vercel React Router preset format

--- react-router.config.js
+++ react-router.config.js
@@ -1,6 +1,6 @@
+// react-router.config.js
 import { vercelPreset } from "@vercel/react-router/vite";
 
-/** @type {import('@react-router/dev/config').Config} */
 export default {
   ssr: true,
   presets: [vercelPreset()],
